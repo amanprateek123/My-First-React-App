89ff0f14778606f99ac07550e6d173147078d747
Aman Prateek <amanpra333@gmail.com>
2019-05-22 12:33:09 +0530

    React App

---
+++ myapp/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the React</h1>
+        <img className="App-logo" src={logo}/>
+      </div>
+    );
+  }
+}
+
+export default App;
